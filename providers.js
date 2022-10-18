a2e6f49cbc95b3f1a5352eb40d0d075ed4ed7eef
tundsta <dev@bulbview.com>
2022-10-18 13:33:44 +0100

    --wip-- [skip ci]

--- providers.js
+++ providers.js
@@ -1,5 +1,7 @@
-import 'dotenv/config';
+import "dotenv/config";
 import { ethers } from "ethers";
 
 const infuraUrl = `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`
 const provider = new ethers.providers.JsonRpcProvider(infuraUrl);
+
+console.log("Current block number", provider.getBlockNumber());
