f439d8bbaf402c03178635d89aa3648f09a5f2d9
tundsta <dev@bulbview.com>
2022-10-18 12:07:27 +0100

    --wip-- [skip ci]

--- providers.js
+++ providers.js
@@ -1,5 +1,5 @@
+import 'dotenv/config';
 import { ethers } from "ethers";
 
-const infuraId; 
-const infuraUrl = `https://mainnet.infura.io/v3/${infuraId}`
-const provider = new ethers.providers.JsonRpcProvider(infuraId);
+const infuraUrl = `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`
+const provider = new ethers.providers.JsonRpcProvider(infuraUrl);
