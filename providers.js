50418cfff51447205c42e849f110e4bfe12ed9d5
tundsta <tundsta@bulbview.com>
2022-11-08 11:54:26 +0000

    get current block number

--- providers.js
+++ providers.js
@@ -4,4 +4,4 @@ import { ethers } from "ethers";
 const infuraUrl = `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`
 const provider = new ethers.providers.JsonRpcProvider(infuraUrl);
 
-console.log("Current block number", provider.getBlockNumber());
+console.log("Current block number", await provider.getBlockNumber());
